0eb89b469a8a4c31a31566e52f96f7e2a82ffc51
dara.somnag020@gmail.com <DaraCOOKED>
2026-03-23 09:30:48 +0700

    Hi

---
+++ database/migrations/2026_03_20_010619_create_employees_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string("date of born");
+            $table->date("sex");
+            $table->string("phone");
+            $table->string("email");
+            $table->string("place of born");
+            $table->string("experience");
+            $table->string("shirt");
+            $table->string("status");
+            $table->string("hired_date");
+            $table->string("salary");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employees');
+    }
+};
